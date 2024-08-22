093889c004347c1d6db46c8a8469342762dd04d3
VINOTH371 <rvinothr91rvinothr91@gmail.com>
2024-08-22 21:20:29 +0530

    Add files via upload
    week1 assignments

---
+++ week1/day1/Method.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Method {
+	
+	public void games() {
+		System.out.println("cricket");
+
+	}
+	public void videogames() {
+		System.out.println("pubg");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Method obj= new Method();
+    obj.games();
+    obj.videogames();
+	}
+	
+}
---
+++ week1/day1/Mobile.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String MobileName = "Samsung";
+		String MobileModel = "S22";
+		float MobileSar = 25.67f;
+		int Mobileprice = 25000;
+
+		System.out.println(MobileName + " \n" + MobileModel + " \n" + MobileSar + " \n" + Mobileprice);
+
+	}
+
+}
